07c79c453b8f6524c6b2e6be3a467972a7eb6a3d
Michael Sabares <msabares>
2021-03-01 23:12:16 -0600

    Reworking the frontend to remove Typescript support - Michael Sabares

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,32 @@
 <template>
   <div id="app">
-    <search-long-term></search-long-term>
-    <listings></listings>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import SearchLongTerm from "./components/subcategories/SearchLongTerm.vue";
-import Listings from "./components/Listings.vue";
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-@Component({
-  components: {
-    SearchLongTerm,
-    Listings,
-  },
-})
-export default class App extends Vue {}
-</script>
+#nav {
+  padding: 30px;
+}
 
-<style></style>
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
